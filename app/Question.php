9dca24b7541d13eead1bf4a0a9b60a3e379a611c
Munawar <munawaraura@gmail.com>
2020-01-08 15:48:56 +0500

    Generate Question model via artisan model generator

---
+++ app/Question.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    //
+
+
+    protected $fillabale=['title','body'];
+    public function user(){
+        return $this->belongsTo(User::class);
+
+    }
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=str_slug($value);
+    }
+
+
+
+}

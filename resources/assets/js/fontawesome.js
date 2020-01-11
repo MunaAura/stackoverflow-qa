fe83ca0572e845f2d5ed8fa88855d5b02e7910dd
Munawar <munawaraura@gmail.com>
2020-01-12 00:59:01 +0500

    add vote controls to question and answers

---
+++ resources/assets/js/fontawesome.js
@@ -0,0 +1,16 @@
+import fontawsome from '@fortawesome/fontawesome';
+import faCaretUp from '@fortawesome/fontawesome-free-solid/faCaretUp';
+import faCaretDown from '@fortawesome/fontawesome-free-solid/faCaretDown';
+import faStar from '@fortawesome/fontawesome-free-solid/faStar';
+import faCheck from '@fortawesome/fontawesome-free-solid/facheck';
+
+fontawsome.library.add([faCaretUp, faCaretDown, faCheck, faStar]);
+// import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
+
+// config.autoReplaceSvg = 'nest'
+
+// import { faCaretUp, faCaretDown, faStar, faCheck, faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+// library.add(faCaretUp, faCaretDown, faCheck, faStar, faSpinner);
+
+// dom.watch()
